4406fb5d6d0ba13db86e4ede48801f7015b85f23
Leigh MacDonald <leigh.macdonald@gmail.com>
2018-03-03 05:08:29 -0700

    Added per-weapon binds and loading binds from a config file.

--- README.md
+++ README.md
@@ -4,20 +4,31 @@ TF2 Bind Generator
 Installation
 ------------
 
-The script requires [python](https://www.python.org/downloads/) to be installed. I have only tested
-with 3.3+, but 2.7 may also work.
+The script only requires [python](https://www.python.org/downloads/) to be installed. It uses no external dependencies.
+I have only tested with python 3.3+, but 2.7 may also work.
 
 
 Usage
 -----
 
 For this to function you must have a key bound to reload the newly updated
-binds generated on the fly. Add a bind like the following to your autoexec.cfg file, replacing
-the bound key to one of your choosing.
+binds generated on the fly. Add a bind like the following to your autoexec.cfg file or the appropriate cfg 
+file for your configuration, replacing the bound key to one of your choosing. I would recommend not using autoexec.cfg
+if possible due to not wanting to re-execute all of it each time, but it will work well enough for most users. Having a 
+custom "binds.cfg" for example could be a good choice.
 
     bind f1 "exec log_parser"
     
-If you don't add a key to reload the log_parser.cfg file you will never get updated player names.
+If you don't add a key to reload the log_parser.cfg file you will never get updated player names. So this is 
+very important and probably the most likely area to encounter a problem with the script.
+
+Additionally, you must also add the following launch options to TF2:
+
+     -condebug -conclearlog
+     
+This will enable the required output of tf2 log messages to the default log path. -conclearlog is optional but
+it will clear the log file upon startup so the log file should not get too large.
+ 
 
     usage: tf2_bind_gen.py [-h] [--log_path LOG_PATH] [--config_path CONFIG_PATH]
                        [--bind_key BIND_KEY] [--test TEST]
@@ -38,3 +49,45 @@ too set the --bind_key option to the key of your choosing. For example:
     tf2_bind_gen.py --bind_key f4
     
 Once running you can use your two binds to reload the log_parser.cfg and to execute your bind.
+
+Customizing Your Sick Memes
+---------------------------
+
+You can customize the binds used by creating or editing the binds.txt file. The format is 1 bind per line and 
+has the following variables which can be used: player, victim, weapon. Some examples are below:
+
+    Get rekt {victim}!
+    [generic] Why so mad {victim}?
+    [generic] {player} rekt {victim} LOL!
+    [market_gardener.crit] Try looking up next time {victim}!
+    [tf_projectile_rocket.crit] EZ Crit! Thanks {victim}!
+    [tf_projectile_rocket] Thanks for the farm {victim}!
+    [world] world > {victim}
+    
+You can specify binds for specific weapon kills and whether they are a crit or not. These keys correspond to the 
+weapon names you see in the console kill log. I don't have a list of all of these, you can check your console logs if 
+you don't know the name of something you want to use. See above for examples of crit and non-crit weapon binds. If no
+[key] is defined, it will be defined as [generic] for you by default.
+
+If you are using an [Australium weapon](https://wiki.teamfortress.com/wiki/Australium), there is no way to know if a 
+kill is a crit or not, all kills are considered a crit according to the console log. So if you want to match a australium
+weapon, make sure to always add ".crit" to the bind key configuration.
+
+If you are using a file other than binds.txt you can change the default path to your binds by specifying the --binds 
+flag value.
+ 
+    tf2_bind_gen.py --binds binds_custom.txt
+
+Will this get me VAC banned?
+----------------------------
+
+Short answer: No. 
+
+Less short answer: No, all this script does is read and parse the tf2 log file which is a plain text file. 
+It then write a new .cfg. There is no functionality to read live TF2 data from memory, attaching to 
+running process or DLL injections happening at all. 
+
+Example
+-------
+
+![Example 1](https://raw.githubusercontent.com/leighmacdonald/tf2_bind_gen/master/example/screen_1.jpg)
--- tf2_bind_gen.py
+++ tf2_bind_gen.py
@@ -1,21 +1,16 @@
 import re
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 from os import environ
 from os.path import isfile, join
-from itertools import cycle
 import logging
+from random import choice
 
-BIND_TEMPLATES = [
-    'Get rekt {}!',
-    'Why so mad {}?'
-]
-
-KillMsg = namedtuple("KillMsg", ['player', 'victim', 'weapon'])
+KillMsg = namedtuple("KillMsg", ['player', 'victim', 'weapon', 'crit'])
 
 
 class LogParser(object):
     #  NAME killed NAME with GUN.
-    _re_kill = re.compile(r"^(.+)\skilled\s(.+)\swith\s(.+)\.|\. \(crit\)$")
+    _re_kill = re.compile(r"^(.+)\skilled\s(.+)\swith\s(.+)(\.|\. \(crit\))$")
 
     #  NAME connected
     _re_connected = re.compile(r"^(.+)\sconnected$")
@@ -23,12 +18,15 @@ class LogParser(object):
     #  Disconnecting from abandoned match server
     _re_disconnect = re.compile(r"^Disconnecting from abandoned match server$")
 
-    def __init__(self, log_path, cfg_path, bind_key):
+    _re_bind_key = re.compile(r"^\[(.+?)\](.+?)$")
+
+    def __init__(self, log_path, cfg_path, bind_key, binds_file):
         self.log_path = log_path
         self.cfg_path = cfg_path
         self.bind_key = bind_key
-        self.templates = cycle(BIND_TEMPLATES)
         self.username = None
+        self.default_bind_key = "generic"
+        self.templates = self.read_binds(binds_file)
 
     def parse_log(self, line):
         if self.username is None:
@@ -43,25 +41,55 @@ class LogParser(object):
         else:
             match = self._re_kill.search(line)
             if match:
-                msg = KillMsg(*match.groups())
+                msg_args = list(match.groups())
+                msg_args[-1] = True if "crit" in msg_args[-1] else False
+                msg = KillMsg(*msg_args)
                 if msg.player == self.username:
                     logging.info(msg)
                     self.write_cfg(msg)
+                    return msg
+
+    def read_binds(self, file_name):
+        found = 0
+        binds = defaultdict(list)
+        for line in open(file_name).readlines():
+            real_line = line.strip()
+            if real_line not in binds:
+                match_key = self._re_bind_key.search(real_line)
+                if match_key:
+                    key, raw_msg = match_key.groups()
+                    msg = raw_msg.strip()
+                else:
+                    key = self.default_bind_key
+                    msg = real_line
+                binds[key].append(msg)
+                found += 1
+        logging.info("Loaded {} binds".format(found))
+        return binds
 
     def write_cfg(self, msg: KillMsg):
         with open(self.cfg_path, 'w+') as cfg:
-            template = next(self.templates)
-            output_str = template.format(msg.victim)
             cfg.write('echo "Loaded log_parser.cfg"\n')
-            cfg.write('bind {} "say {}\n'.format(self.bind_key, output_str))
+            cfg.write('bind {} "say {}\n'.format(self.bind_key, self.gen_message(msg)))
+
+    def gen_message(self, msg: KillMsg):
+        key = "{}.crit".format(msg.weapon) if msg.crit else "{}".format(msg.weapon)
+        try:
+            template = choice(self.templates[key])
+        except IndexError:
+            template = choice(self.templates[self.default_bind_key])
+        output_str = template.format(victim=msg.victim, player=msg.player, weapon=msg.weapon)
+        return output_str
 
     def start(self):
         for line in self.tail():
             self.parse_log(line)
 
     def read_file(self, log_file):
-        for line in open(log_file).readlines():
-            self.parse_log(line)
+        for line in open(log_file, encoding='utf8').readlines():
+            msg = self.parse_log(line)
+            if msg:
+                print(self.gen_message(msg))
 
     def tail(self):
         first_call = True
@@ -95,16 +123,21 @@ if __name__ == "__main__":
     import argparse
 
     logging.basicConfig(level=logging.DEBUG)
-    program_files_path = environ['PROGRAMFILES(X86)']
+    try:
+        program_files_path = environ['PROGRAMFILES(X86)']
+    except KeyError:
+        program_files_path = environ['PROGRAMFILES']
     log_path_default = join(program_files_path, r"Steam\steamapps\common\Team Fortress 2\tf\console.log")
     config_path_default = join(program_files_path, r"Steam\steamapps\common\Team Fortress 2\tf\cfg\log_parser.cfg")
     parser = argparse.ArgumentParser(description='TF2 Log Tail Parser')
     parser.add_argument('--log_path', default=log_path_default)
     parser.add_argument('--config_path', default=config_path_default, help="Path to the .cfg file to be generated")
     parser.add_argument('--bind_key', default="f2", help="Keyboard shortcut used for chat bind")
-    parser.add_argument('--test', type=bool, default=False, help="Test parsing your existing log files")
+    parser.add_argument('--test', action='store_true', help="Test parsing your existing log files")
+    parser.add_argument('--binds', default="binds.txt", help="Path to your custom binds file.")
     args = parser.parse_args()
-    parser = LogParser(args.log_path, args.config_path, args.bind_key)
+
+    parser = LogParser(args.log_path, args.config_path, args.bind_key, args.binds)
     if args.test:
         parser.read_file(log_path_default)
     else:
